ea2c1f364597ad82d4a376cc53325f28b538d744
Jean BaMaung <jbamaung17@gmail.com>
2018-02-02 11:28:50 +0000

    Made song files

---
+++ specs/songs_spec.rb
@@ -0,0 +1,21 @@
+require('minitest/autorun')
+require('minitest/rg')
+require("pry")
+
+require_relative('../songs.rb')
+
+class SongsTest < MiniTest::Test
+
+def setup
+
+@song = Song.new("Africa", "Toto")
+
+end
+
+
+
+
+
+
+
+end
